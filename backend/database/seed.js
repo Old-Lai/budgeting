54b5b390ba8b643cac24181a1efdb995ef9a8fec
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-02-28 11:35:16 -0800

    change TIMESTAMP variable to TIMESTAMPTZ this includes timezone config when storeing timestamp which defults to UTC time, eliminates time differences caused by system settings

--- backend/database/seed.js
+++ backend/database/seed.js
@@ -31,8 +31,8 @@ async function createTables() {
                 email VARCHAR(255) UNIQUE NOT NULL,
                 username VARCHAR(255) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
                 enabled BOOLEAN DEFAULT TRUE
             );
 
