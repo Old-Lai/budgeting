7aa4987fad2ef3190da9341665953232814c3778
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-03-12 13:19:16 -0700

    added a back button to not found page

--- frontend/src/pages/error/NotFound.jsx
+++ frontend/src/pages/error/NotFound.jsx
@@ -3,6 +3,7 @@ export default function NotFound() {
     <main>
       <h1>Page Not Found</h1>
       <p>Sorry, but the page you were trying to view does not exist.</p>
+      <button onClick={() => window.history.back()}>{"<-"} Lets go back to where it was safe</button>
     </main>
   )
 }
