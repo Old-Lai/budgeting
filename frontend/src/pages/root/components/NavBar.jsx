b331b71b5412d34dcd6119195de4262beb686651
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-03-12 13:20:01 -0700

    removed unused classname

--- frontend/src/pages/root/components/NavBar.jsx
+++ frontend/src/pages/root/components/NavBar.jsx
@@ -4,11 +4,9 @@ import './navbar.sass';
 export default function NavBar() {
   return (
     <nav>
-      <ul className>
+      <ul>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
         <Link to="/tools">Tools</Link>
         <Link to="/profile">Account</Link>
       </ul>
